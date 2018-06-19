e87ed65fd9d50c9ee42368567010c3545a94e034
katherine <kathermar25@gmail.com>
2018-06-19 00:04:35 -0400

    primer commit

---
+++ public/js/socket-chat.js
@@ -0,0 +1,60 @@
+var socket = io();
+var params = new URLSearchParams(window.location.search);
+
+
+
+if (!params.has('nombre') || !params.has('sala')) {
+    window.location = 'index.html';
+    throw new Error('el nombre y sala son necesarios')
+}
+
+var usuario = {
+    nombre: params.get('nombre'),
+    sala: params.get('sala')
+
+}
+
+socket.on('connect', function() {
+    console.log('Conectado al servidor');
+
+    socket.emit('entrarChat', usuario, function(resp) {
+        console.log('Usuario Conectados', resp);
+    });
+});
+
+// escuchar
+socket.on('disconnect', function() {
+
+    console.log('Perdimos conexión con el servidor');
+
+});
+
+
+// Enviar información
+//socket.emit('crearMensaje', {
+//  usuario: 'Fernando',
+// mensaje: 'Hola Mundo'
+//}, function(resp) {
+//  console.log('respuesta server: ', resp);});
+
+// Escuchar información
+socket.on('crearMensaje', function(mensaje) {
+
+    console.log('Servidor:', mensaje);
+
+});
+
+//escuchar cambios del usuario
+// cuando usuario entra o sale del chat
+
+socket.on('listaPersonas', function(personas) {
+
+    console.log(personas);
+
+});
+
+// mensajes privados
+
+socket.on('mensajePrivado', function(mensaje) {
+    console.log('Mensaje Privado:', mensaje);
+})
